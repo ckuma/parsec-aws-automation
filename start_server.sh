c47aa7a5d3e4d4b24dfa5382f16aa492f518e46d
Mehdy Chaillou <51863237+ckuma@users.noreply.github.com>
2021-02-05 04:27:31 +0000

    Up-to-date version of the script
    - shebang reference to bash should go through env on any system (env will pick the most fitting bash on the system)
    - filters have a different format (`Name=tag:Name,Values=...`)
    - removed empty spaces
    
    (Thanks for the script!)

--- start_server.sh
+++ start_server.sh
@@ -1,11 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 GAMING_INSTANCE_NAME="YOUR GAMING RIG NAME GOES HERE"
 LAUNCH_TEMPLATE="YOUR LAUNCH TEMPLATE ID GOES HERE"
 
-ami=`aws ec2 describe-images --filters Name=name,Values="$GAMING_INSTANCE_NAME" --output text --query 'Images[*].{ID:ImageId}'`
+ami=`aws ec2 describe-images --filters Name=tag:Name,Values="$GAMING_INSTANCE_NAME" --output text --query 'Images[*].{ID:ImageId}'`
 
 echo "Launching new instance with AMI id: $ami"
 aws ec2 run-instances \
     --launch-template LaunchTemplateId=$LAUNCH_TEMPLATE,Version=1 --image-id $ami
-
